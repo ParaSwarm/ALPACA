807cabbbb5ca7fa00e579681f9a9711c18bcb1a6
melicent <melicentking@gmail.com>
2014-10-23 18:40:49 -0500

    add contact list table

---
+++ ALPACA/ALPACA.Database/Contacts.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Contacts]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [email] NVARCHAR(50) NOT NULL UNIQUE, 
+    [lastname] NVARCHAR(50) NULL, 
+    [firstname] NVARCHAR(50) NULL
+)

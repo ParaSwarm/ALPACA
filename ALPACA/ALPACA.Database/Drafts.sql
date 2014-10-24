897ea42bcc80f0bc3a9154ddd266f4f4122aa5bb
melicent <melicentking@gmail.com>
2014-10-24 14:28:56 -0500

    add drafts table

---
+++ ALPACA/ALPACA.Database/Drafts.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Drafts]
+(
+	[dID] INT NOT NULL PRIMARY KEY,
+	[uID] NVARCHAR(128) NOT NULL, 
+    [draftBody] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [FK_Drafts_AspNetUsers] FOREIGN KEY ([uID]) REFERENCES [AspNetUsers]([Id])
+
+)

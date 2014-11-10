aaa11ecd52ca637cae9284c09d64f03e2a9ac22c
ParaSwarm <kodamn@gmail.com>
2014-11-10 06:22:22 -0600

    Committing things I think may have been missed.

---
+++ ALPACA/ALPACA/Entities/EmailDraft.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ALPACA.Entities
+{
+    public class EmailDraft
+    {
+        public virtual int Id { get; set; }
+        public virtual int UserId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Body { get; set; }
+    }
+}
